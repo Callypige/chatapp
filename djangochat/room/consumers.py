10fa3a98ceb635672114ff3f1c72915a3ea40f88
Snourry <30350968+Existrace@users.noreply.github.com>
2023-08-02 10:20:52 +0200

    Async consumer + show one message

--- djangochat/room/consumers.py
+++ djangochat/room/consumers.py
@@ -9,30 +9,40 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_group_name = "room_%s" % self.room_name
 
         # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Retrieve messages from Websocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        data = json.loads(text_data)
+        message = data["message"]
+        username = data["username"]
+        room = data["room"]
 
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+                "username": username,
+                "room": room,
+            },
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
+        username = event["username"]
+        room = event["room"]
 
         # Send message to Websocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(
+            text_data=json.dumps(
+                {"message": message, "username": username, "room": room}
+            )
+        )
